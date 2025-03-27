d1930476be3b8f70c170c4d84bed79aad8266107
Ishwari <doleishwari@gmail.com>
2025-03-27 12:20:54 +0530

    update settings

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -106,7 +106,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Backend.wsgi.application'
 
-
+#  This is my comment
 # Channels
 ASGI_APPLICATION = 'Backend.asgi.application'
 
@@ -129,18 +129,24 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         # 'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'AIInterview',
+#        'USER': 'postgres',
+#        'PASSWORD': 'root',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        # 'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'AIInterview',
-       'USER': 'postgres',
-       'PASSWORD': 'root',
-       'HOST': 'localhost',
-       'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
-
  
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
